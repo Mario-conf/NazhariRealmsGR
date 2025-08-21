88564bed9e6802096a40ee2a73e7f3f08f7783f7
Mario AV <mario04asir@gmail.com>
2025-08-21 12:01:32 +0000

    This error happens because next-intl can’t find your config file, which

--- i18n.ts
+++ i18n.ts
@@ -1,14 +1,5 @@
 import {getRequestConfig} from 'next-intl/server';
-
-export default getRequestConfig(async ({locale}) => {
-  try {
-    return {
-      messages: (await import(`./messages/${locale}.json`)).default
-    };
-  } catch (error) {
-    console.error('Error loading messages:', error);
-    return {
-      messages: {} // Provide a fallback empty object
-    };
-  }
-});
+ 
+export default getRequestConfig(async ({locale}) => ({
+  messages: (await import(`./messages/${locale}.json`)).default
+}));
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,17 @@
 import createMiddleware from 'next-intl/middleware';
-import { locales, localePrefix } from './navigation';
  
 export default createMiddleware({
   // A list of all locales that are supported
-  locales,
-  localePrefix,
+  locales: ['en', 'es', 'de', 'it', 'fr'],
  
   // Used when no locale matches
   defaultLocale: 'es'
 });
  
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(de|en|es|it|fr)/:path*']
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
+    
