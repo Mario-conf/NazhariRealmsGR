79d7940f7eb937e7c7702d7328415c2bb51ebbe4
Mario AV <mario04asir@gmail.com>
2025-08-22 12:17:59 +0000

    <!-- Remove the container if you want to extend the Footer to full width

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,19 +1,95 @@
 import { useTranslations } from 'next-intl';
 import { Link } from '@/navigation';
+import { Facebook, Twitter, Instagram, Linkedin, MountainIcon, Home, Mail, Phone } from 'lucide-react';
 
 export function Footer() {
   const t = useTranslations('Footer');
 
   return (
-    <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 bg-gray-900 text-gray-400">
-      <p className="text-xs">{t('copyright')}</p>
-      <div className="sm:ml-auto flex gap-4 sm:gap-6">
-        <Link className="text-xs hover:underline hover:text-white underline-offset-4" href="#">
-          {t('terms')}
-        </Link>
-        <Link className="text-xs hover:underline hover:text-white underline-offset-4" href="#">
-          {t('privacy')}
-        </Link>
+    <footer className="bg-gray-800 text-white text-center lg:text-left">
+      {/* Social media section */}
+      <div className="flex justify-center items-center lg:justify-between p-4 border-b border-gray-700">
+        <div className="mr-12 hidden lg:block">
+          <span>{t('social_connect')}</span>
+        </div>
+        <div className="flex justify-center">
+          <a href="#!" className="mr-6 text-gray-400 hover:text-white">
+            <Facebook />
+          </a>
+          <a href="#!" className="mr-6 text-gray-400 hover:text-white">
+            <Twitter />
+          </a>
+          <a href="#!" className="mr-6 text-gray-400 hover:text-white">
+            <Instagram />
+          </a>
+          <a href="#!" className="text-gray-400 hover:text-white">
+            <Linkedin />
+          </a>
+        </div>
+      </div>
+
+      {/* Links section */}
+      <div className="container mx-auto p-6 text-center md:text-left">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          {/* Company info */}
+          <div className="mx-auto md:mx-0">
+            <h6 className="uppercase font-bold mb-4 flex items-center justify-center md:justify-start gap-2">
+              <MountainIcon />
+              Alpine-Hike Club
+            </h6>
+            <p className="text-gray-400">
+              {t('company_description')}
+            </p>
+          </div>
+
+          {/* Useful Links */}
+          <div className="mx-auto">
+            <h6 className="uppercase font-bold mb-4">{t('useful_links_title')}</h6>
+            <p className="mb-2">
+              <Link href="/routes" className="text-gray-400 hover:text-white">{t('routes_link')}</Link>
+            </p>
+            <p className="mb-2">
+              <Link href="/rules" className="text-gray-400 hover:text-white">{t('rules_link')}</Link>
+            </p>
+            <p className="mb-2">
+              <Link href="/blog" className="text-gray-400 hover:text-white">{t('blog_link')}</Link>
+            </p>
+            <p>
+              <Link href="/reviews" className="text-gray-400 hover:text-white">{t('reviews_link')}</Link>
+            </p>
+          </div>
+          
+           {/* Legal Links */}
+          <div className="mx-auto">
+            <h6 className="uppercase font-bold mb-4">{t('legal_title')}</h6>
+            <p className="mb-2">
+              <a href="#!" className="text-gray-400 hover:text-white">{t('privacy')}</a>
+            </p>
+            <p>
+              <a href="#!" className="text-gray-400 hover:text-white">{t('terms')}</a>
+            </p>
+          </div>
+
+
+          {/* Contact */}
+          <div className="mx-auto">
+            <h6 className="uppercase font-bold mb-4">{t('contact_title')}</h6>
+            <p className="flex items-center justify-center md:justify-start mb-2 text-gray-400">
+              <Home className="mr-3 h-5 w-5"/> Granada, España
+            </p>
+            <p className="flex items-center justify-center md:justify-start mb-2 text-gray-400">
+              <Mail className="mr-3 h-5 w-5"/> info@alpinehike.com
+            </p>
+            <p className="flex items-center justify-center md:justify-start text-gray-400">
+              <Phone className="mr-3 h-5 w-5"/> +34 123 456 789
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Copyright section */}
+      <div className="text-center p-4 bg-black/20 text-gray-400 text-sm">
+        {t('copyright')}
       </div>
     </footer>
   );
