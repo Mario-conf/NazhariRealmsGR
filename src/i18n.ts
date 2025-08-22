fa83b3e9c4acd783eb83c00851ccaae707771f7a
Mario AV <mario04asir@gmail.com>
2025-08-22 10:58:16 +0000

    digest: '506264680' }
    Error: [Error: Expected a suspended thenable. Thi

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,15 +1,16 @@
-import {notFound} from 'next-intl';
-import {getRequestConfig} from 'next-intl/server';
+import { getRequestConfig } from 'next-intl/server';
+import { notFound } from 'next/navigation';
  
 // Can be imported from a shared config
 const locales = ['en', 'es', 'de', 'it', 'fr'];
  
-export default getRequestConfig(async ({locale}) => {
+export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
+  if (!locales.includes(locale as any)) {
+    notFound();
+  }
  
   return {
-    locale,
     messages: (await import(`./messages/${locale}.json`)).default
   };
 });
