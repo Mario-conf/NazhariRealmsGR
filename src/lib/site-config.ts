fb4d59f384d854203cfdcfc26b778bdf4924ecb6
Mario Acosta Vargas <mariocastell907@gmail.com>
2025-08-30 09:40:30 +0000

    https://www.facebook.com/profile.php?id= 61580029080711  mete eso de fac

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -1,4 +1,4 @@
-import { FaWhatsapp, FaSpotify, FaStrava, FaInstagram } from "react-icons/fa6";
+import { FaWhatsapp, FaSpotify, FaStrava, FaInstagram, FaFacebook } from "react-icons/fa6";
 
 export const siteConfig = {
     name: "Nazhari Realms GR",
@@ -7,6 +7,7 @@ export const siteConfig = {
     socialLinks: [
         { name: 'WhatsApp', href: 'https://whatsapp.com/channel/0029Vb2kFn442Dcl8E0bUT2k', Icon: FaWhatsapp, colorClass: 'hover:text-[#25D366]' },
         { name: 'Spotify', href: 'https://open.spotify.com/playlist/3GhlwrQQJzdBgh0mod4dnm?si=OqOYa4oTT4etDBWWs6-YPw&pi=c6LqInruTKaO4', Icon: FaSpotify, colorClass: 'hover:text-[#1DB954]' },
+        { name: 'Facebook', href: 'https://www.facebook.com/profile.php?id=61580029080711', Icon: FaFacebook, colorClass: 'hover:text-[#1877F2]' },
         { name: 'Strava', href: 'https://www.strava.com/clubs/nazhari-realms-gr', Icon: FaStrava, colorClass: 'hover:text-[#FC4C02]' },
         { name: 'Instagram', href: 'https://instagram.com/nazharirealmsgr', Icon: FaInstagram, colorClass: 'hover:text-[#E4405F]' },
     ]
