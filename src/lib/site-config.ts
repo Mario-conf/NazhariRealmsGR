fc1712538b8fbf3b5f36bbe8901c6ddc33f9aff7
Mario Acosta Vargas <mariocastell907@gmail.com>
2025-08-30 20:46:07 +0000

    strava update

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -8,7 +8,7 @@ export const siteConfig = {
         { name: 'WhatsApp', href: 'https://whatsapp.com/channel/0029Vb2kFn442Dcl8E0bUT2k', Icon: FaWhatsapp, colorClass: 'hover:text-[#25D366]' },
         { name: 'Spotify', href: 'https://open.spotify.com/playlist/3GhlwrQQJzdBgh0mod4dnm?si=OqOYa4oTT4etDBWWs6-YPw&pi=c6LqInruTKaO4', Icon: FaSpotify, colorClass: 'hover:text-[#1DB954]' },
         { name: 'Facebook', href: 'https://www.facebook.com/profile.php?id=61580029080711', Icon: FaFacebook, colorClass: 'hover:text-[#1877F2]' },
-        { name: 'Strava', href: 'https://www.strava.com/clubs/nazhari-realms-gr', Icon: FaStrava, colorClass: 'hover:text-[#FC4C02]' },
+        { name: 'Strava', href: 'https://g.page/r/CaqIcWCpANMnEBI/review', Icon: FaStrava, colorClass: 'hover:text-[#FC4C02]' },
         { name: 'Instagram', href: 'https://instagram.com/nazharirealmsgr', Icon: FaInstagram, colorClass: 'hover:text-[#E4405F]' },
     ]
 }
